ed7c0af0bfb2f1ec7b65a7fe90de4c1bc4ff1de8
adazem009 <info@adazem.com>
2024-07-27 16:20:28 +0000

    TB8504: Use local cryptfshw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Crypto
 BOARD_USES_QCOM_DECRYPTION := true
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
