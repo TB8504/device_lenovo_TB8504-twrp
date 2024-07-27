646667cd0542d9a99cb46193f7379ddd2c06f169
adazem009 <info@adazem.com>
2024-07-27 22:29:23 +0000

    TB8504: Update fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,8 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+
 # Workaround for error copying vendor files to recovery ramdisk
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
